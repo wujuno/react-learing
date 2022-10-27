39c7c66c8b48c51b43344c60ae90287a0a09de8e
wujuno <86146780+wujuno@users.noreply.github.com>
2022-10-27 12:43:34 +0900

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [ counter, setValue ] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev)=> prev +1);
+  const onChange = (event) => setKeyword(event.target.value);
+  const iRunOnlyOne = ()=> {
+    console.log("i run only once");
+  }
+  useEffect(iRunOnlyOne, []);
+  useEffect(()=>{
+    if(keyword !== "" && keyword.length>5) {
+      console.log("SEARCH FOR", keyword)
+    }
+  }, [keyword]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value={keyword} type="text" placeholder="Search here..." onChange={onChange} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
